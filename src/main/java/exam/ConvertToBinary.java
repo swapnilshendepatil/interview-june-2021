58c3ffde7638bc4048de214daf2102e95124bfc2
Swapnil Shende <47530398+swapnilshendepatil@users.noreply.github.com>
2021-06-22 13:06:40 +0530

    Update ConvertToBinary.java

--- src/main/java/exam/ConvertToBinary.java
+++ src/main/java/exam/ConvertToBinary.java
@@ -4,6 +4,19 @@ public class ConvertToBinary {
 
   public String toBinary(final int n) {
     //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
+    int bin[] = new int [40];
+  	int ind = 0 ;
+
+  	try{
+  		while(n >= 0){
+  			bin[ind++]==num%2;
+  			num = num/2;
+  		}
+  	}
+  	catch(Exception e){
+  		System.out.println("so far it doesn't still supports to convert the negative number into binary"+ e.getMessage());
+
+  	}
     return "";
   }
 }
